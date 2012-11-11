81d054db3db0f4bb0dc32f41ded476e06730b20f
Doomape <vilijam19@yahoo.com>
2012-11-11 01:56:31 +0100

    stared working on the project

--- master/header.php
+++ master/header.php
@@ -2,14 +2,12 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="../style.css">
 
 <title>Feydom</title>
 </head>
 
 <body>
-<!--header-->
-<?php include 'master/header.php'; ?>
-
+<div id="header"></div>
 </body>
 </html>
