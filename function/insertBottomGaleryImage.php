64f639d4e76d6744fb63ceb308c833da4dd405b4
Micevski Kosta <micevski.kosta@gmail.com>
2012-11-25 20:55:12 +0100

    koki push

---
+++ function/insertBottomGaleryImage.php
@@ -0,0 +1,27 @@
+<?php
+		$itemvalue=($_POST["data"]);
+			$con = mysql_connect("localhost","root","");
+			if (!$con)
+			  {
+			  die('Could not connect: ' . mysql_error());
+			  }
+	        $idP=0;
+			$idM=0;
+			mysql_select_db("feydom", $con);
+			$id = mysql_query("SELECT max(id) FROM left_sidebar");
+		    while($row = mysql_fetch_array($id))
+		    {
+				 $idP=$row['max(id)'];
+			}
+			$idmc = mysql_query("SELECT max(idmc) FROM main_content");
+		    while($row = mysql_fetch_array($idmc))
+		    {
+				 $idM=$row['max(idmc)'];
+			}
+		    $idM++;
+		//	echo $idP;
+		//	echo "--------";
+			//echo $idM;
+			mysql_query("INSERT INTO main_content (id, idmc, contentBottomURL, galeryCheck) VALUES('".$idP."', '".$idM."','".$itemvalue."','true')");
+			mysql_close($con);
+?>
